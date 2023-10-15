642005431ec785863dbc86ea7aaede2b337fced4
Basavraj <basavrajbirajdar2001@gmail.com>
2023-10-15 23:33:59 +0530

    Added ratelimiter and proxy

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -4,7 +4,9 @@ dotenv.config();
 
 module.exports = {
     PORT : process.env.PORT,
-    saltRound : process.env.saltRound,
+    SALTROUND : process.env.saltRound,
     JWT_SECRET_KEY :process.env.JWT_SECRET_KEY,
-    JWT_EXPIRY : process.env.JWT_EXPIRY
+    JWT_EXPIRY : process.env.JWT_EXPIRY,
+    FLIGHT_SERVICE : process.env.FLIGHT_SERVICE,
+    BOOKING_SERVICE : process.env.BOOKING_SERVICE
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 const express = require('express');
 
+const { rateLimit }=require('express-rate-limit');
+
+const { createProxyMiddleware }=require('http-proxy-middleware');
+
 const { Serverconfig ,logger } = require('./config');
 
 const app = express();
 
 const router = require('./routers');
+const serverConfig = require('./config/server-config');
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', router);
+const limiter = rateLimit({
+	windowMs: 2 * 60 * 1000, // 15 minutes
+	limit: 4, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+	// store: ... , // Use an external store for more precise rate limiting
+});
+
+app.use('/api',limiter, router);
+
+app.use('/flightsService', createProxyMiddleware({ target: Serverconfig.FLIGHT_SERVICE, changeOrigin: true , pathRewrite: {'^/flightsService' : '/'}}));
+
+app.use('/bookingService', createProxyMiddleware({ target: serverConfig.BOOKING_SERVICE, changeOrigin: true , pathRewrite: {'^/bookingService' : '/'}}));
 
 app.listen(Serverconfig.PORT, () => {
     console.log(`Server is up and Runing on ${Serverconfig.PORT}`);
